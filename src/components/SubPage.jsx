f5e5f1d94d5943fcb5e8d6033d337d606f3d9788
Jasmine Henry <Jasminehenry356@gmail.com>
2023-04-24 16:57:33 -0400

    First Commit

---
+++ src/components/SubPage.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react"
+import { UserStatus } from "../App.js"
+import Button from "./Button.jsx"
+
+export default function SubPage () {
+
+    return(
+        <>
+            <h2>SubPage</h2>
+            <Button />
+        </>
+    )
+}
